fb9effeafbdaca8f05d60344e755239dd047cb0c
Bartholomé Gili <bartholome.gili@etu.univ-lyon1.fr>
2020-11-26 19:55:36 +0100

    starts of the request-promise and cheerio based scraper structure

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const NautiljonScraper = require("./lib/nautiljonScraper");
 
 let nautiljonScraper = new NautiljonScraper();
-nautiljonScraper.search("one piece").then(console.log);
+nautiljonScraper.search("one piece", "anime");
